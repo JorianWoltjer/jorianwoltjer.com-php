c7107ad7d6d6fc9a34c78bfb044157e6124013d0
JorianWoltjer <jorianwoltjer@hotmail.com>
2022-02-08 20:56:34 +0100

    make relative path into absolute path

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once("include/header.php"); ?>
 
     <h1 class="my-4 index-page"><?= randomCode("Hello, I am Jorian Woltjer"); ?></h1>
-    <img src="img/logo.png" class="index-page">
+    <img src="/img/logo.png" class="index-page">
     <h1 class="my-4 index-page"><?= randomCode("Welcome to my website!"); ?></h1>
 
 <?php require_once("include/footer.php"); ?>
