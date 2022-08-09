99f04b3256851c875d352bd001bbf4a02e985b37
JorianWoltjer <jorianwoltjer@hotmail.com>
2022-08-09 13:50:44 +0200

    Update contact page

--- contact.php
+++ contact.php
@@ -4,46 +4,40 @@ $meta_title = "Contact";
 $meta_description = "A list of accounts you can use to contact me. Discord, Github, email, etc. If you have any questions or want to reach out, feel free to use any of these accounts.";
 require_once("include/header.php"); ?>
 
+<style nonce="<?= $nonce ?>">
+    .fa-flag, .fa-discord {
+        font-size: 95%;
+    }
+</style>
+
     <h1 class="my-4"><code>Contact</code></h1>
 
     <div class="buttons">
         <a href="https://www.youtube.com/c/J0R1AN/" target="_blank"><div class="button red">
-            <div class="button-icon"><i class="fab fa-youtube"></i></div>
+            <div class="button-icon"><i class="fa-brands fa-youtube"></i></div>
             <div class="button-text">YouTube</div>
         </div></a>
-        <a href="https://www.reddit.com/user/JorianID" target="_blank"><div class="button orange">
-            <div class="button-icon"><i class="fab fa-reddit-alien"></i></div>
-            <div class="button-text">Reddit</div>
+        <a href="https://ctftime.org/user/83640" target="_blank"><div class="button orange">
+            <div class="button-icon"><i class="fa-solid fa-flag"></i></div>
+            <div class="button-text">CTFtime</div>
         </div></a>
         <a href="https://twitter.com/J0R1AN" target="_blank"><div class="button blue">
-            <div class="button-icon"><i class="fab fa-twitter"></i></div>
+            <div class="button-icon"><i class="fa-brands fa-twitter"></i></div>
             <div class="button-text">Twitter</div>
         </div></a>
-        <a id="copy" href="" onclick="return copy_discord(this)"  data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Copied!" aria-label="Copied!"><div class="button discord-blue">
-            <div class="button-icon"><i class="fab fa-discord"></i></div>
+        <a href="https://discordapp.com/users/298743112421867521" target="_blank"><div class="button discord-blue">
+            <div class="button-icon"><i class="fa-brands fa-discord"></i></div>
             <div class="button-text">Jorian#0135</div>
         </div></a>
         <a href="https://stackoverflow.com/users/10508498" target="_blank"><div class="button light-green">
-            <div class="button-icon"><i class="fab fa-stack-overflow"></i></div>
+            <div class="button-icon"><i class="fa-brands fa-stack-overflow"></i></div>
             <div class="button-text">Stack Overflow</div>
         </div></a>
         <a href="https://github.com/JorianWoltjer" target="_blank"><div class="button gray">
-            <div class="button-icon"><i class="fab fa-github"></i></div>
+            <div class="button-icon"><i class="fa-brands fa-github"></i></div>
             <div class="button-text">Github</div>
         </div></a>
     </div>
     <p class="button"><i class="fa-solid fa-envelope"></i><a href="mailto: jorianwoltjer@hotmail.com">jorianwoltjer@hotmail.com</a></p>
-
-<script nonce="<?=$nonce?>">
-    function copy_discord(e) {
-        const tooltip = new bootstrap.Tooltip(document.getElementById("copy"));
-        navigator.clipboard.writeText(e.innerText);
-        tooltip.show();
-        setTimeout(function() {
-            tooltip.dispose();
-        }, 2000);
-        return false;
-    }
-</script>
 
 <?php require_once("include/footer.php"); ?>
