fba1bc2143c775cb337e8994c1fd21bc13f753ac
Jorian Woltjer <jorianwoltjer@hotmail.com>
2023-03-25 10:46:36 +0100

    Add IP rate-limiting to add_view

--- blog/add_view.php
+++ blog/add_view.php
@@ -1,8 +1,23 @@
 <?php require_once("../include/all.php");
 
-// If this gets abused, add rate limiting on IP basis (per post)
-
-// Increment view count if not a bot user agent and post is visible
-if (!preg_match('/bot\W/i', $_SERVER["HTTP_USER_AGENT"])) {
-    sql_query("UPDATE posts SET views = views+1 WHERE id = ? AND hidden IS NULL", [$_GET["id"]]);
+if (preg_match('/bot\W/i', $_SERVER["HTTP_USER_AGENT"])) {
+    die("Bots cannot add views");
 }
+if (!isset($_GET["id"]) || sql_query("SELECT id FROM posts WHERE id = ? AND hidden IS NULL", [$_GET["id"]])->num_rows == 0) {
+    die("Invalid post ID");
+}
+
+// I don't even want the possibility of recovering your IP address
+// Trying to crack this won't be fun
+$unique = $_SERVER["REMOTE_ADDR"] . date("Y-m-d H:00:00");  // Should be unique per user, per hour, but unspoofable
+$unique_hash = hash_pbkdf2("sha256", $unique, "salt_4e7be08c", 10000, 32, true);
+
+$count_response = sql_query("SELECT count(id) FROM views WHERE post_id = ? AND unique_hash = ?", [$_GET["id"], $unique_hash]);
+
+if ($count_response->fetch_row()[0] >= 5) {
+    http_response_code(429);
+    die("Already viewed 5 times this hour");
+}
+sql_query("INSERT INTO views (post_id, unique_hash) VALUES (?, ?)", [$_GET["id"], $unique_hash]);
+// views table is reset every hour by cron.php, so we need to remember the count only
+sql_query("UPDATE posts SET views = views+1 WHERE id = ?", [$_GET["id"]]);
--- include/cron.php
+++ include/cron.php
@@ -1,2 +1,5 @@
 <?php
 // This script is ran every hour using a Cron Job
+
+// Clear views table (only used for rate-limiting)
+sql_query("DELETE FROM views");
